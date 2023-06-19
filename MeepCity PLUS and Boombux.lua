9f0fe5e59264aaf424451c4d518dba4364f970ec
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-06-19 12:06:05 +0300

    Update MeepCity PLUS and Boombux.lua

--- MeepCity PLUS and Boombux.lua
+++ MeepCity PLUS and Boombux.lua
@@ -1,2 +1,5 @@
+if not game:IsLoaded() then
+  game.Loaded:Wait()
+end
 game:GetService("Players").LocalPlayer:SetAttribute("BoomBox",true)
 game:GetService("Players").LocalPlayer:SetAttribute("PLUS",true)
