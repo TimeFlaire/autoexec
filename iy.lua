8eb1092aaebbfee47a414156dc403c2efe84603a
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-06-19 12:07:56 +0300

    Update iy.lua

--- iy.lua
+++ iy.lua
@@ -1,2 +1,5 @@
+if not game:IsLoaded() then
+  game.Loaded:Wait()
+end
 -- loadstring(game:HttpGet('https://raw.githubusercontent.com/TimeFlaire/IY9/main/UwU'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/TimeFlaire/IY9/main/UwU593'))()
