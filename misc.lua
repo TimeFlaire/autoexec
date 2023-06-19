1d1a0bf636c15a7db0e047ec5e6f79caf580dd4a
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-06-19 12:08:10 +0300

    Update misc.lua

--- misc.lua
+++ misc.lua
@@ -1,3 +1,7 @@
+if not game:IsLoaded() then
+  game.Loaded:Wait()
+end
+
 for i,v in pairs(game:GetService("Workspace").Building.Eliminate:GetDescendants()) do
  if v.ClassName == "TouchTransmitter" then
      v:Destroy()
