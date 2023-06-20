a43ce30b75cdd10ca4bda598db7444d39db8bff4
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-06-20 20:46:09 +0300

    Update ogui.lua

--- ogui.lua
+++ ogui.lua
@@ -1,4 +1,1 @@
-if not game:IsLoaded() then
-  game.Loaded:Wait()
-end
-loadstring(game:HttpGet("https://pastebin.com/raw/UXmbai5q", true))()
+loadstring(game:HttpGet("https://pastebin.com/raw/UXmbai5q",true))()
