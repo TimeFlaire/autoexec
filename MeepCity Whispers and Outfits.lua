71acb939ca24a48b0991d6e651ee47097f9dc836
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-06-26 23:16:00 +0300

    Update MeepCity Whispers and Outfits.lua

--- MeepCity Whispers and Outfits.lua
+++ MeepCity Whispers and Outfits.lua
@@ -1,5 +1,14 @@
 if not game:IsLoaded() then
   game.Loaded:Wait()
 end
+local Players = service("Players")
+local MarketplaceService = service("MarketplaceService")
+local ReplicatedStorage = service("ReplicatedStorage")
+local HttpService = service("HttpService")
+
+local Constants = require(ReplicatedStorage:WaitForChild("Constants"))
+local Connection = ReplicatedStorage:WaitForChild("Connection")
+local ConnectionEvent = ReplicatedStorage:WaitForChild("ConnectionEvent")
+
 Constants.STATS.WHISPER_MAX_CHARACTERS = 999999
 Constants.STATS.WHISPER_MAX_CHARACTERS = 999999
