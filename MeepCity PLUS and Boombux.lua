7ca6d7df6e82580e025d3c294c6c082327f98d16
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-06-27 16:42:23 +0300

    Update MeepCity PLUS and Boombux.lua

--- MeepCity PLUS and Boombux.lua
+++ MeepCity PLUS and Boombux.lua
@@ -1,5 +1,18 @@
 if not game:IsLoaded() then
   game.Loaded:Wait()
 end
+
 game:GetService("Players").LocalPlayer:SetAttribute("BoomBox",true)
 game:GetService("Players").LocalPlayer:SetAttribute("PLUS",true)
+
+local Players = service("Players")
+local MarketplaceService = service("MarketplaceService")
+local ReplicatedStorage = service("ReplicatedStorage")
+local HttpService = service("HttpService")
+
+local Constants = require(ReplicatedStorage:WaitForChild("Constants"))
+local Connection = ReplicatedStorage:WaitForChild("Connection")
+local ConnectionEvent = ReplicatedStorage:WaitForChild("ConnectionEvent")
+
+Constants.STATS.WHISPER_MAX_CHARACTERS = 999999
+Constants.STATS.WHISPER_MAX_CHARACTERS = 999999
