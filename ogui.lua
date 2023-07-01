d031668c29489e898ef16e65b0ffd59c80c57e3c
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-07-01 20:56:31 +0300

    Update ogui.lua

--- ogui.lua
+++ ogui.lua
@@ -1,3 +1,8 @@
+if not game:IsLoaded() then
+  game.Loaded:Wait()
+end
+if game:IsLoaded() then
 loadstring(game:HttpGet("https://pastebin.com/raw/UXmbai5q",true))()
 task.wait(.1)
 game:GetService("CoreGui").ScreenGui.Mods.Text = " "
+end
