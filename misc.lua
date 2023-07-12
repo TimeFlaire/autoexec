641e7d9d2f355f9ac4ecb5d548d10ce13aa4462c
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-07-12 18:44:26 +0300

    Update misc.lua

--- misc.lua
+++ misc.lua
@@ -1,14 +1,16 @@
+--Put in autoexec
 if not game:IsLoaded() then
   game.Loaded:Wait()
 end
-if game:IsLoaded() then
-if game.PlaceId == 61846006 then
-for i,v in pairs(game:GetService("Workspace").Building.Eliminate:GetDescendants()) do
+
+if game.PlaceId == 61846006 then --To prevent errors with other games when on autoexec, the game will be checked
+for i,v in pairs(game:GetService("Workspace").Building.Eliminate:GetDescendants()) do --Check's game.Workspace.Building.Eliminate
  if v.ClassName == "TouchTransmitter" then
-     v:Destroy()
+     v:Destroy() -- Destroys all touch transmitters, making you immortal
  end
 end
-pcall(function()
+-- Break the game as Simon, you will instantly win
+pcall(function() -- pcall to prevent the loop breaking
 	while true do
     	game:GetService("ReplicatedStorage").ServerEvents.SimonCommand:InvokeServer(true,true,1)
         task.wait(.1)
@@ -18,5 +20,4 @@ pcall(function()
 end)
 
 		
-end
 end
