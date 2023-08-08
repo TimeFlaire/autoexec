adacde4deb4eb801a0e3119283cbd5ad76fb69b2
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-08-08 20:52:20 +0300

    Update graphics.lua

--- graphics.lua
+++ graphics.lua
@@ -2,7 +2,7 @@ if not game:IsLoaded() then
   game.Loaded:Wait()
 end
 
-sethiddenproperty(Lighting, "Technology", "Future")
+sethiddenproperty(game:WaitForChild("Lighting", "Technology", "Future")
 
 game:WaitForChild("Workspace").DescendantAdded:Connect(function(v)
 if v:IsA("SpotLight") or v:IsA("SurfaceLight") or v:IsA("PointLight") then
