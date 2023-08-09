17ef444a7e3f9c73a9fa47eb1835d508cd52af10
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-08-09 15:28:12 +0300

    Create megahideandseekcoins.lua

---
+++ megahideandseekcoins.lua
@@ -0,0 +1,13 @@
+
+if game.PlaceID == "5708035517" then
+
+while task.wait() do
+    for n,m in pairs(Game:GetService("Workspace"):GetDescendants()) do
+        pcall(function()
+        if m.name == "Coin" then
+            m.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+        end)
+    end
+end
+end
