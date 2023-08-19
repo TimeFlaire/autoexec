9082dc7e9991b661019732bad5066549e210d86c
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-08-19 22:22:37 +0300

    Update ogui.lua

--- ogui.lua
+++ ogui.lua
@@ -2,7 +2,5 @@ if not game:IsLoaded() then
   game.Loaded:Wait()
 end
 if game:IsLoaded() then
-loadstring(game:HttpGet("https://pastebin.com/raw/UXmbai5q",true))()
-task.wait(.1)
-game:GetService("CoreGui").ScreenGui.Mods.Text = " "
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TimeFlaire/BringMePotatoJuice/main/ogui.lua",true))()
 end
