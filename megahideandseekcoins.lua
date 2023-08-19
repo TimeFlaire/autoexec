0822962ad872bfda159c64b9cda7da2c4355d77a
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-08-19 20:07:30 +0300

    Update megahideandseekcoins.lua

--- megahideandseekcoins.lua
+++ megahideandseekcoins.lua
@@ -9,4 +9,13 @@ while task.wait() do
         end)
     end
 end
+elseif game.PLaceId == 61846006 then
+while true do
+for i,v in pairs(game.Workspace.Coins:GetDescendants()) do
+    if v.Name == "Coin" and not v:IsA("Sound") then
+        v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    end
+end
+task.wait(.1)
+end
 end
