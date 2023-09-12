0abeaf8efc499af5e113796f05e401abc603ffc5
TimeFlaire <87149869+TimeFlaire@users.noreply.github.com>
2023-09-12 18:11:49 +0300

    Update misc.lua

--- misc.lua
+++ misc.lua
@@ -5,6 +5,24 @@ end
 
 if game.PlaceId == 61846006 then --To prevent errors with other games when on autoexec, the game will be checked
 game.StarterGui:SetCoreGuiEnabled("PlayerList",true)
+task.spawn(function()
+while true do
+    pcall(function()
+        task.wait(.001)
+        for _, player in pairs(game.Players:GetChildren()) do
+            if player:IsA("Player") and player.Character then
+                for _, part in pairs(player.Character:GetChildren()) do
+                    if part:IsA("BasePart") and part.Transparency == 1 and part.Name ~= "HumanoidRootPart" then
+                        part.Transparency = 0
+                        warn("Player " .. player.Name .. "'s part " .. part.Name .. " is invisible, changing to visible")
+                    end
+                end
+            end
+        end
+    end)
+end
+
+		end)
 coroutine.resume(coroutine.create(function()
     for i,v in pairs(workspace:FindPartsInRegion3(
         Region3.new(Vector3.new(-20.799148559570312,88.90079498291016,-13.818876266479492),
